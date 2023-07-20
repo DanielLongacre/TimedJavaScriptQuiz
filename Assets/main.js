af4869458b93a28678b2a2a7dee7a620f3ca9f15
Daniel Longacre <danieljlongacre@gmail.com>
2023-07-20 18:54:42 -0400

    Added question2 function in JS

--- Assets/main.js
+++ Assets/main.js
@@ -25,7 +25,7 @@ function question1() {
         <br>
     `
     var answer = document.querySelector("#correct");
-    var incorrect = document.querySelector("wrong");
+    var incorrect = document.querySelector("#wrong");
 
     answer.addEventListener("click", function(event) {
         event.preventDefault();
@@ -40,8 +40,41 @@ function question1() {
                 <p>Wrong!</p>
             `;
         }
+        question2();
     });
 
 }
 
-// question1();
+//Question 2 function
+function question2() {
+    var main = document.querySelector("#display");
+    main.innerHTML = ``;
+    main.innerHTML += `
+        <h1>The condition in an if / else statement is enclosed with _______:</h1>
+        <ol>
+            <li><button id="correct">Parenthesis</button></li>
+            <li><button id="wrong">Curly Brackets</button></li>
+            <li><button id="wrong">Quotes</button></li>
+            <li><button id="wrong">Square Brackets</button></li>
+        </ol>
+        <br>
+    `
+    var answer = document.querySelector("#correct");
+    var incorrect = document.querySelector("#wrong");
+
+    answer.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        if(answer) {
+            main.innerHTML += `
+                <p>Correct!</p>
+            `;
+        } 
+        else if (incorrect) {
+            main.innerHTML += `
+                <p>Wrong!</p>
+            `;
+        }
+    });
+
+}
